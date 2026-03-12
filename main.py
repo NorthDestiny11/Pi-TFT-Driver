090d6268771f80bbfbeecf7f4cb91a05333a6ab9
NorthDestiny11 <northdestiny11@gmail.com>
2026-03-12 16:45:54 +1100

    Defined all used pinouts.

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import gpiozero
 from time import sleep
 
+### Do not assign any power pins. This is handled by the display.
+### Only SCK, MOSI, MISO, CE0, CE1, GPIO 25, GPIO 24 are to be touched.
+
+V3_3_1 = 1
+V3_3_2 = 17
+
+V5_1 = 2
+V5_2 = 4
+
+GND_1 = 6 
+GND_2 = 9
+GND_3 = 14
+GND_4 = 20
+GND_5 = 25
+
 MOSI = 10
 MISO = 9
 SCLK = 11
+
+CE0 = 8
+CE1 = 7
+
+IO24 = 18
+IO25 = 22
